2b639cb3dac9e5dc6c420dc6c11b32ab56b58c61
jeffseverson2001 <jeff@zealouscreations.com>
2021-05-18 18:30:39 -0500

    Tuesday Start

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+
+let displayCity = $('#city');
+
+
+//  Fetch Weather API
+
+//  Build Data for Fetch Call
+
+//  Add Elements for top-weather-data
+
+//  Add 5 day forcase elements ????? -- Static elements in HTML page
+

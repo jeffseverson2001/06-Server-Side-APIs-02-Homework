a8a228799d7f36b62c39852460df3a1bb1d84a70
jeffseverson2001 <jeff@zealouscreations.com>
2021-05-19 13:24:08 -0500

    Wed Update 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 
 //let displayCity = $('#city');
 
-let weatherAPI = "https://api.openweathermap.org/data/2.5/weather?q=Minneapolis&APPID=326a02beef126b1aa5dec68624560327";
+//let weatherAPI = "https://api.openweathermap.org/data/2.5/weather?q=Minneapolis&units=imperial&APPID=326a02beef126b1aa5dec68624560327";
+//let weatherZipAPI = "https://api.openweathermap.org/data/2.5/weather?zip=55303,US&units=imperial&appid=326a02beef126b1aa5dec68624560327";
+let weatherAPI = "https://api.openweathermap.org/data/2.5/forecast?q=Minneapolis&units=imperial&appid=326a02beef126b1aa5dec68624560327";
 
 console.log(weatherAPI);
 
@@ -13,10 +15,9 @@ fetch(weatherAPI)
     })
     .then(function (data) {
         console.log(data);
-        for (let i=0; i < data.length; i++){
-            console.log(data.length);
-            
-        }
+        //console.log(data.coord.lon);
+        console.log(data.name);
+        //console.log(data.weather[0].icon);
     });
 
 //  Build Data from Fetch Call

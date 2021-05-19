b28938aa2eb8b429131999e82ac56350927647b2
jeffseverson2001 <jeff@zealouscreations.com>
2021-05-19 12:08:16 -0500

    Wed Early

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,25 @@
 
-let displayCity = $('#city');
+//let displayCity = $('#city');
 
+let weatherAPI = "https://api.openweathermap.org/data/2.5/weather?q=Minneapolis&APPID=326a02beef126b1aa5dec68624560327";
+
+console.log(weatherAPI);
 
 //  Fetch Weather API
+fetch(weatherAPI)
+    .then(function (response) {
+        console.log(response.status);
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        for (let i=0; i < data.length; i++){
+            console.log(data.length);
+            
+        }
+    });
 
-//  Build Data for Fetch Call
+//  Build Data from Fetch Call
 
 //  Add Elements for top-weather-data
 

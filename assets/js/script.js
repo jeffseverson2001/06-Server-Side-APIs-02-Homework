e3be32a6429002fcc579982d81c0e13e4dd54217
jeffseverson2001 <jeff@zealouscreations.com>
2021-05-21 22:50:48 -0500

    Friday Work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,24 @@
 
-//let displayCity = $('#city');
+//let currentDisplayedCity = $('#displayedCity');
+//let searchButton = $('#citySearchButton').val();
+let searchBox = $('#searchBox').val();
 
-let weatherAPI = "https://api.openweathermap.org/data/2.5/weather?q=Minneapolis&units=imperial&APPID=326a02beef126b1aa5dec68624560327";
+if(searchBox === ""){
+    searchBox = "Atlanta";
+    seachForCity(searchBox);
+}
+
+
+
+//console.log("Search Button: " + searchButton);
+console.log("Search Box: " + searchBox);
+
+//let weatherAPI = "https://api.openweathermap.org/data/2.5/weather?q="+searchBox+"&units=imperial&APPID=326a02beef126b1aa5dec68624560327";
 //let weatherZipAPI = "https://api.openweathermap.org/data/2.5/weather?zip=55303,US&units=imperial&appid=326a02beef126b1aa5dec68624560327";
-let weatherForcastAPI = "https://api.openweathermap.org/data/2.5/forecast?q=Minneapolis&units=imperial&appid=326a02beef126b1aa5dec68624560327";
+//let weatherForcastAPI = "https://api.openweathermap.org/data/2.5/forecast?q="+searchBox+"&units=imperial&appid=326a02beef126b1aa5dec68624560327";
 
-console.log(weatherAPI);
+//console.log(weatherAPI);
+
 
 //  Fetch City Weather API
 function getCityWeather(weatherAPI){
@@ -32,22 +45,67 @@ function getForcastWeather(weatherForcastAPI){
     .then(function (data) {
         console.log(data);
         //console.log(data.coord.lon);
-        console.log(data.name);
+        console.log(data.city.name);
         //console.log(data.weather[0].icon);
     });
 }
 
-//  Get Local Stored Items
+//  Get Last Searches from Local Storage
 function getLocalItems() {
     let recentCities = localStorage.getItem("recentCities");
     return JSON.parse(recentCities);
 }
 
-//  Build Forcast Data from Fetch Call
+//  Write Last 8 Searches to Local Storage
+function writeLocalItems(lastCitiesSearched){
+    prevent.preventDefault();
+
+    //  Add Last Cities Object Here
+
+    console.log(lastCitiesSearched);
+    localStorage.setItem("lastCitiesSearched", JSON.stringify(lastCitiesSearched));
+}
+
+
+//  Build 5 Day Forcast Data from Fetch Call
 
 //  Build City Search Data from Fetch Call
+function seachForCity(searchCity){
+
+    let weatherAPI = "https://api.openweathermap.org/data/2.5/weather?q="+searchCity+"&units=imperial&APPID=326a02beef126b1aa5dec68624560327";
+    let weatherForcastAPI = "https://api.openweathermap.org/data/2.5/forecast?q="+searchCity+"&units=imperial&appid=326a02beef126b1aa5dec68624560327";
+
+    console.log("City Search: " + searchCity);
+
+    getCityWeather(weatherAPI);
+    getForcastWeather(weatherForcastAPI);
+
+    //let cityResults = "";
+
+    //return JSON.parse(cityResults);
+}
 
 //  Add Elements for top-weather-data
+//let currentCity = "";
 
 //  Add 5 day forcase elements ????? -- Static elements in HTML page
+
+//  Build City List for Search Box --- JQuery UI
+
+//  Search Button Listener
+
+$(document).ready(function() {
+    $('#citySearchButton').click( function() {
+        console.log("Click: " + $('#searchBox').val());
+        seachForCity($('#searchBox').val());
+    });
+});
+
+
+
+
+
+
+
+
 

e49d3f6f32802e605f70eb3bf245c786208a1d15
jeffseverson2001 <jeff@zealouscreations.com>
2021-05-22 18:02:06 -0500

    Final Commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # 06-Server-Side-APIs-02-Homework
 Homework Week 6
+
+## Description
+
+Weather Dashboard
+
+- To work with 3rd Party APIs functions with Javascript.
+- Get current weather and forcast from openweather.
+
+
+##  Useage
+
+- Loads weather for Atlanta on startup.
+- Enter in a new city and click submit button.
+
+Published Github Site URL:
+https://jeffseverson2001.github.io/06-Server-Side-APIs-02-Homework/
+
+![alt Password Generator](assets/06-server-side-apis-homework-demo.png)
+
+
+##  Credits
+
+University of Minnesota Boot Camp:
+https://umn.bootcampcontent.com/University-of-Minnesota-Boot-Camp
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 //let currentDisplayedCity = $('#displayedCity');
 let searchBox = $('#searchBox').val();
 
+
 console.log("Search Box: " + searchBox);
 
 //let DateTime = luxon.DateTime;
@@ -12,7 +13,7 @@ console.log("Current Date: " + currentDate);
 async function getCityWeather(searchCity) {
 
     let weatherAPI = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&units=imperial&APPID=326a02beef126b1aa5dec68624560327";
-    
+
     await fetch(weatherAPI)
         .then(function (response) {
             console.log("Fetch Repsonse: " + response.status);
@@ -26,19 +27,16 @@ async function getCityWeather(searchCity) {
             console.log(data.name);
             console.log(data.main.temp);
             console.log(data.weather[0].icon);
-            
+
+            $(".top-city-weather").append("<h1>" + data.name + " (" + currentDate + ") </h1>");
             $(".top-city-weather").append("<img id=\"weather-icon\" src=\"https://openweathermap.org/img/w/" + data.weather[0].icon + ".png\" alt=\"Weather icon\" /> ");
             $(".top-city-weather").append(
-                "<h1>" + data.name + " (" + currentDate + ") </h1>",
-                "<div class=\"current-font\">Temp: " + data.main.temp + " &#8457;</div>",
-                "<div class=\"current-font\">Wind: " + data.wind.speed + " MPH</div>",
-                "<div class=\"current-font\">Humidity: " + data.main.humidity + " %</div>",
-                "<div class=\"current-font\">UV Index: " + data.main.temp + "</div>"
+                "<div class=\"current-font\">Currently:  " + data.weather[0].description + "</div>",
+                "<div class=\"current-font\">Temp:  " + data.main.temp + " &#8457;</div>",
+                "<div class=\"current-font\">Wind:  " + data.wind.speed + " MPH</div>",
+                "<div class=\"current-font\">Humidity:  " + data.main.humidity + " %</div>",
+                "<div class=\"current-font\">UV Index:  " + data.main.temp + "</div>"
             );
-            
-            //$("#weather-icon").attr("src", "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
-            
-            
         });
 }
 
@@ -53,11 +51,32 @@ async function getForcastWeather(searchCity) {
             return response.json();
         })
         .then(function (data) {
+            $(".bottom-city-forcast").html("");
             console.log(data);
             //console.log(data.coord.lon);
             console.log(data.city.name);
-            //console.log(data.weather[0].icon);
-            return data;
+
+            let lastDateCheck = "";
+            //var forcastObject = {};
+
+            for (i = 0; i < data.list.length; i++) {
+                let rawDateCheck = data.list[i].dt_txt;
+                let dateCheck = rawDateCheck.substr(0, 10);
+                console.log(lastDateCheck + " -- " + dateCheck)
+                if (dateCheck !== lastDateCheck) {
+                    console.log("HERE" + i);                      
+                    $("<div class=\"forcast-box\">").appendTo(".bottom-city-forcast");
+                    $(".forcast-box").append("<h2>" + dateCheck + "</h2>");
+                    $(".forcast-box").append("<img id=\"weather-icon\" src=\"https://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png\" alt=\"Weather icon\" /> ");
+                    $(".forcast-box").append(
+                        "<div class=\"forcast-font\">Currently:  " + data.list[i].weather[0].description + "</div>",
+                        "<div class=\"forcast-font\">Temp:  " + data.list[i].main.temp + " &#8457;</div>",
+                        "<div class=\"forcast-font\">Wind:  " + data.list[i].wind.speed + " MPH</div>",
+                        "<div class=\"forcast-font\">Humidity:  " + data.list[i].main.humidity + " %</div>"
+                    );
+                }
+                lastDateCheck = dateCheck;
+            }
         });
 }
 
@@ -78,73 +97,23 @@ function writeLocalItems(lastCitiesSearched) {
 }
 
 
-//  Build 5 Day Forcast Data from Fetch Call
-
-//  Build City Search Data from Fetch Call
-/*
-function seachForCity(searchCity) {
-
-    let weatherAPI = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&units=imperial&APPID=326a02beef126b1aa5dec68624560327";
-    let weatherForcastAPI = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchCity + "&units=imperial&appid=326a02beef126b1aa5dec68624560327";
-
-    console.log("City Search: " + searchCity);
-
-    let cityWeather = getCityWeather(weatherAPI);
-    let cityForcast = getForcastWeather(weatherForcastAPI);
-
-    console.log("Temp: " + cityWeather);
-    //currentCityWeather(cityWeather);
-}
-*/
-
-//  Add Elements for top-weather-data
-/*
-function currentCityWeather(cityWeather) {
-    console.log("CW: " + cityWeather);
-    $('.top-city-weather').html(
-        "<h1>" + cityWeather.name + " (" + currentDate + ") </h1>",
-        "<div class=\"current-font\">Temp: " + cityWeather + "</div>",
-        "<div class=\"current-font\">Wind: </div>",
-        "<div class=\"current-font\">Humidity: </div>",
-        "<div class=\"current-font\">UV Index: </div>"
-    );
-    //$('.top-city-weather').html("<h2>TEST YOP</h2>");
-    //$(".top-city-weather").attr({
-    //    "class" : "city-weather-header",
-    //});
-
-}
-*/
-
-//let currentCity = "";
-
-//  Add 5 day forcase elements ????? -- Static elements in HTML page
-
-//  Build City List for Search Box --- JQuery UI
-
-
-
 
 
 //  Search Button Listener
 $(document).ready(function () {
     $('#citySearchButton').click(function () {
         console.log("Click: " + $('#searchBox').val());
-        //seachForCity($('#searchBox').val());
         getCityWeather($('#searchBox').val());
+        getForcastWeather($('#searchBox').val());
     });
 });
 
+
 if (searchBox === "") {
     searchBox = "Atlanta";
-    //seachForCity(searchBox);
+    getCityWeather(searchBox);
+    getForcastWeather(searchBox);
 }
-
-
-
-
-
-
 
 
 
